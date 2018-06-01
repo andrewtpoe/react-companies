1b21b691885d6fe1866de9addc25711dadc6ea7c
Andrew T. Poe <andrewtpoe@gmail.com>
2018-05-31 22:26:53 -0400

    [misc/bug-fixes] resolves some issues found in production (#16)

--- src/elements/Span/index.js
+++ src/elements/Span/index.js
@@ -10,106 +10,26 @@ const modifiers = {
   }),
   fontSizeSM: ({ theme }) => ({
     styles: css`
-      font-size: ${theme.dimensions.fontSizeXS};
-      line-height: calc(${theme.dimensions.fontSizeXS} * 1.25);
-
-      @media all and (${theme.dimensions.screenSM}) {
-        font-size: ${theme.dimensions.fontSizeSM};
-        line-height: calc(${theme.dimensions.fontSizeSM} * 1.25);
-      }
-
-      @media all and (${theme.dimensions.screenMD}) {
-        font-size: ${theme.dimensions.fontSizeSM};
-        line-height: calc(${theme.dimensions.fontSizeSM} * 1.25);
-      }
-
-      @media all and (${theme.dimensions.screenLG}) {
-        font-size: ${theme.dimensions.fontSizeSM};
-        line-height: calc(${theme.dimensions.fontSizeSM} * 1.25);
-      }
-
-      @media all and (${theme.dimensions.screenXL}) {
-        font-size: ${theme.dimensions.fontSizeSM};
-        line-height: calc(${theme.dimensions.fontSizeSM} * 1.25);
-      }
+      font-size: ${theme.dimensions.fontSizeSM};
+      line-height: calc(${theme.dimensions.fontSizeSM} * 1.25);
     `,
   }),
   fontSizeMD: ({ theme }) => ({
     styles: css`
-      font-size: ${theme.dimensions.fontSizeSM};
-      line-height: calc(${theme.dimensions.fontSizeSM} * 1.25);
-
-      @media all and (${theme.dimensions.screenSM}) {
-        font-size: ${theme.dimensions.fontSize};
-        line-height: calc(${theme.dimensions.fontSize} * 1.25);
-      }
-
-      @media all and (${theme.dimensions.screenMD}) {
-        font-size: ${theme.dimensions.fontSize};
-        line-height: calc(${theme.dimensions.fontSize} * 1.25);
-      }
-
-      @media all and (${theme.dimensions.screenLG}) {
-        font-size: ${theme.dimensions.fontSize};
-        line-height: calc(${theme.dimensions.fontSize} * 1.25);
-      }
-
-      @media all and (${theme.dimensions.screenXL}) {
-        font-size: ${theme.dimensions.fontSize};
-        line-height: calc(${theme.dimensions.fontSize} * 1.25);
-      }
+      font-size: ${theme.dimensions.fontSize};
+      line-height: calc(${theme.dimensions.fontSize} * 1.25);
     `,
   }),
   fontSizeLG: ({ theme }) => ({
     styles: css`
-      font-size: ${theme.dimensions.fontSize};
-      line-height: calc(${theme.dimensions.fontSize} * 1.25);
-
-      @media all and (${theme.dimensions.screenSM}) {
-        font-size: ${theme.dimensions.fontSizeLG};
-        line-height: calc(${theme.dimensions.fontSizeLG} * 1.25);
-      }
-
-      @media all and (${theme.dimensions.screenMD}) {
-        font-size: ${theme.dimensions.fontSizeLG};
-        line-height: calc(${theme.dimensions.fontSizeLG} * 1.25);
-      }
-
-      @media all and (${theme.dimensions.screenLG}) {
-        font-size: ${theme.dimensions.fontSizeLG};
-        line-height: calc(${theme.dimensions.fontSizeLG} * 1.25);
-      }
-
-      @media all and (${theme.dimensions.screenXL}) {
-        font-size: ${theme.dimensions.fontSizeLG};
-        line-height: calc(${theme.dimensions.fontSizeLG} * 1.25);
-      }
+      font-size: ${theme.dimensions.fontSizeLG};
+      line-height: calc(${theme.dimensions.fontSizeLG} * 1.25);
     `,
   }),
   fontSizeXL: ({ theme }) => ({
     styles: css`
-      font-size: ${theme.dimensions.fontSizeLG};
-      line-height: calc(${theme.dimensions.fontSizeLG} * 1.25);
-
-      @media all and (${theme.dimensions.screenSM}) {
-        font-size: ${theme.dimensions.fontSizeXL};
-        line-height: calc(${theme.dimensions.fontSizeXL} * 1.25);
-      }
-
-      @media all and (${theme.dimensions.screenMD}) {
-        font-size: ${theme.dimensions.fontSizeXL};
-        line-height: calc(${theme.dimensions.fontSizeXL} * 1.25);
-      }
-
-      @media all and (${theme.dimensions.screenLG}) {
-        font-size: ${theme.dimensions.fontSizeXL};
-        line-height: calc(${theme.dimensions.fontSizeXL} * 1.25);
-      }
-
-      @media all and (${theme.dimensions.screenXL}) {
-        font-size: ${theme.dimensions.fontSizeXL};
-        line-height: calc(${theme.dimensions.fontSizeXL} * 1.25);
-      }
+      font-size: ${theme.dimensions.fontSizeXL};
+      line-height: calc(${theme.dimensions.fontSizeXL} * 1.25);
     `,
   }),
 };
