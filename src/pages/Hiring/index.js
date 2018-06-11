e6ee5629e695a4166274538d903f3043deb6b291
Andrew Poe <apoe@decisiv.net>
2018-06-11 18:03:57 -0400

    [feat] adds react helmet

--- src/pages/Hiring/index.js
+++ src/pages/Hiring/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Helmet } from 'react-helmet';
 
 import { Article } from 'blocks';
 
@@ -8,10 +9,19 @@ import { H2 } from 'elements';
 
 function Hiring() {
   return (
-    <Article>
-      <H2 modifiers={['borderBottom']}>Hiring</H2>
-      <HiringCompanies />
-    </Article>
+    <Fragment>
+      <Helmet>
+        <title>React Companies | Hiring</title>
+        <meta
+          name="description"
+          content={`A list of companies that use React JS and are hiring.`}
+        />
+      </Helmet>
+      <Article>
+        <H2 modifiers={['borderBottom']}>Hiring</H2>
+        <HiringCompanies />
+      </Article>
+    </Fragment>
   );
 }
 
--- src/pages/Industries/index.js
+++ src/pages/Industries/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Helmet } from 'react-helmet';
 
 import { Article } from 'blocks';
 
@@ -8,10 +9,19 @@ import { H2 } from 'elements';
 
 function Industries() {
   return (
-    <Article>
-      <H2 modifiers={['borderBottom']}>Industries</H2>
-      <IndustriesList />
-    </Article>
+    <Fragment>
+      <Helmet>
+        <title>React Companies | Industries</title>
+        <meta
+          name="description"
+          content="Companies in these industries use React JS."
+        />
+      </Helmet>
+      <Article>
+        <H2 modifiers={['borderBottom']}>Industries</H2>
+        <IndustriesList />
+      </Article>
+    </Fragment>
   );
 }
 
